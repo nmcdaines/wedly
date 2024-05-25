aa8f37bdd9033a9cbe12b8556f0e5c2bc6bc16c3
Nate Daines <nate@thedaines.org>
2024-05-25 21:08:47 +1000

    update namespace to wedly

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@wedly/ui/card";
+import { Code } from "@wedly/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@wedly/ui/button";
 
 function Gradient({
   conic,
