d8c37026281afa9a7d9060ef756fd9347fc70009
Nate Daines <nathaniel.daines@franklin.ai>
2024-05-29 22:38:07 +1000

    wip

--- apps/web/app/preview-2/page.tsx
+++ apps/web/app/preview-2/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import {
   DndContext,
   DragEndEvent,
@@ -9,38 +9,76 @@ import {
   UniqueIdentifier,
 } from '@dnd-kit/core'
 import { nanoid } from 'nanoid'
-import { Droppable, Draggable } from '../../components/dragAndDrop'
+import {
+  Droppable,
+  Draggable,
+  DroppableDivider,
+} from '../../components/dragAndDrop'
+import { Text, Image } from 'lucide-react'
+
+type ComponentType = {
+  id: string
+  icon: string | Element
+  label: string
+}
+
+const COMPONENTS: ComponentType[] = [
+  { id: 'text', icon: <Text className="w-6 h-6" />, label: 'Text' },
+  { id: 'image', icon: <Image className="w-6 h-6" />, label: 'Image' },
+]
 
 export default function Page() {
   const [elements, setElements] = useState<any[]>([])
   const [activeId, setActiveId] = useState<UniqueIdentifier | undefined>()
 
-  const draggable = <Draggable id={'text'}>Go ahead, drag me.</Draggable>
+  const [content, setContent] = useState<any[]>([
+    {
+      id: 'one',
+      type: 'text',
+      content: 'The quick brown fox jumped over the lazy dog',
+    },
+    { id: 'two', type: 'text', content: 'One' },
+    { id: 'three', type: 'text', content: 'Two' },
+    { id: 'four', type: 'text', content: 'Three' },
+    { id: 'five', type: 'text', content: 'Four' },
+    { id: 'six', type: 'text', content: 'Five' },
+  ])
+
+  const activeComponent: ComponentType | undefined = useMemo(() => {
+    return COMPONENTS.find((component) => component.id === activeId)
+  }, [activeId])
 
   return (
     <div style={{ height: '100vh' }}>
       {JSON.stringify(elements)}
       <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
-        {draggable}
-        <Droppable id="one">Drop Here (One)</Droppable>
-        <Droppable id="two">Drop Here (Two)</Droppable>
-        <Droppable id="three">Drop Here (Three)</Droppable>
+        <div className="flex gap-3">
+          {COMPONENTS.map((component) => (
+            <Draggable key={component.id} id={component.id}>
+              <ComponentCard
+                id={component.id}
+                label={component.label}
+                icon={component.icon}
+              />
+            </Draggable>
+          ))}
+        </div>
+
+        {content.map((item) => (
+          <>
+            <div key={item.id}>{item.content}</div>
+            <DroppableDivider key={item.id} id={item.id} />
+          </>
+        ))}
 
         <DragOverlay dropAnimation={null}>
-          {activeId ? (
-            <div
-              className=""
-              style={{
-                display: 'block',
-                background: 'red',
-                borderRadius: '16px',
-                width: '48px',
-                height: '64px',
-              }}
-            >
-              {activeId}
-            </div>
-          ) : null}
+          {activeComponent && (
+            <ComponentCard
+              id={activeComponent.id}
+              label={activeComponent.label}
+              icon={activeComponent.icon}
+            />
+          )}
         </DragOverlay>
       </DndContext>
     </div>
@@ -57,4 +95,13 @@ export default function Page() {
 
     setElements([...elements, { id: nanoid(), el: active.id, over: over.id }])
   }
+}
+
+function ComponentCard({ id, label, icon }: ComponentType) {
+  return (
+    <div className="cursor-pointer bg-slate-700 w-16 h-20 rounded-xl flex flex-col items-center justify-center gap-1 shadow-lg">
+      <div className="text-xl font-black">{icon}</div>
+      <div className="text-sm">{label}</div>
+    </div>
+  )
 }
--- apps/web/components/dragAndDrop.tsx
+++ apps/web/components/dragAndDrop.tsx
@@ -1,20 +1,33 @@
-import { useDraggable, useDroppable } from '@dnd-kit/core'
+import { UniqueIdentifier, useDraggable, useDroppable } from '@dnd-kit/core'
 import { PropsWithChildren } from 'react'
+
+import { Plus, PlusCircle } from 'lucide-react'
 
 export function Droppable(props: PropsWithChildren<{ id: string }>) {
   const { isOver, setNodeRef } = useDroppable({
     id: props.id,
   })
 
-  const style = {
-    opacity: isOver ? 1 : 0.5,
-    width: '100%',
-    background: isOver ? 'blue' : '',
-  }
+  return (
+    <div ref={setNodeRef} className="py-4 px-2">
+      {props.children}
+    </div>
+  )
+}
+
+export function DroppableDivider({ id }: { id: UniqueIdentifier }) {
+  const { isOver, setNodeRef } = useDroppable({ id })
 
   return (
-    <div ref={setNodeRef} style={style}>
-      {props.children}
+    <div ref={setNodeRef} className="py-4 px-2 flex flex-col">
+      <hr
+        className={`transition-all rounded-lg border-4 border-blue-600 ${!isOver ? 'opacity-0' : ''}`}
+      />
+      {isOver && (
+        <div className="bg-blue-400 inline-block p-1 rounded-full border border-2 ml-auto mr-auto -translate-y-2/3">
+          <Plus className="w-4 h-4" />
+        </div>
+      )}
     </div>
   )
 }
